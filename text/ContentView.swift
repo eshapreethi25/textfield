19a974308c2ccf7b48a6f6ef36ca82f1b7ea24b0
eshapreethi <eshapreethi1997@gmail.com>
2023-10-03 21:06:55 -0500

    new testfield

--- text/ContentView.swift
+++ text/ContentView.swift
@@ -8,19 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var EmailID = ""
+    @State private var Password = ""
+    @State private var SendmeEmails = true
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section(header: Text("Create New Profile")) {
+                    Section(header: Text("First Name")) {
+                        
+                        TextField("First Name", text:$firstName)
+                        Section(header: Text("Last Name")) {
+                            TextField("last Name", text:$lastName)
+                            Section(header: Text("Email ID")) {
+                                TextField("Email ID", text:$EmailID)
+                                Section(header: Text("Email ID")) {
+                                    TextField("Password", text:$Password)
+                                    
+                                }
+                            }
+                        }}}
+                Toggle(isOn: $SendmeEmails) {
+                    Text("Send me Emails")
+                }
+                Button("Submit"){
+                }
+                    
+                    
+                }
+            }}
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
         }
-        .padding()
     }
-}
+    
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
