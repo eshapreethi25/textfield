5d8f24793392ca526fa04b749730edbc1838d487
eshapreethi <eshapreethi1997@gmail.com>
2023-10-03 20:40:30 -0500

    Initial Commit

---
+++ text/textApp.swift
@@ -0,0 +1,17 @@
+//
+//  textApp.swift
+//  text
+//
+//  Created by Deepesh Madala on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct textApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
